bdc1fc98656cf17f5f93d02e0f4eb1acf7db6a64
Hyun A Kim <hyuna@Violet.local>
2024-01-04 20:07:55 +0900

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  settings: {},
+  plugins: ['import', 'unused-imports', '@typescript-eslint'],
+  rules: {
+    'no-duplicate-imports': 'warn',
+    'import/no-duplicates': ['warn', {considerQueryString: true}],
+    'unused-imports/no-unused-imports': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'spaced-comment': 'warn',
+    'react-native/no-inline-styles': 'off',
+  },
 };
